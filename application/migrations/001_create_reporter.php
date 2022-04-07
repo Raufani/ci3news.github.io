421eb3c5eea96406759e97db62ae24aedc3672f3
aiableHQ <54804394+aiableHQ@users.noreply.github.com>
2022-04-08 05:51:16 +0700

    update 0.3

--- application/migrations/001_create_reporter.php
+++ application/migrations/001_create_reporter.php
@@ -2,6 +2,7 @@
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 class Migration_create_reporter extends CI_Migration {
     public function up() {
+        $attributes = array('ENGINE' => 'MyISAM');
         $fields = array(
             'id' => array(
                 'type' => 'INT',
@@ -16,13 +17,12 @@ class Migration_create_reporter extends CI_Migration {
             'id_news' => array(
                 'type' => 'INT',
                 'constraint' => 11,
-                'unsigned' => TRUE,
             ),
         );
         $this->dbforge->add_field($fields);
         $this->dbforge->add_key('id');
         //$this->dbforge->add_field('CONSTRAINT FOREIGN KEY (id_news) REFERENCES ci_news(ne_id)');
-        $this->dbforge->create_table('reporter');
+        $this->dbforge->create_table('reporter', FALSE, $attributes);
 
         $data = array(
             array('reporter_name' => "raufani",
@@ -30,9 +30,9 @@ class Migration_create_reporter extends CI_Migration {
             array('reporter_name' => "Abdul",
             'id_news' => "38"),
          );
-         //$this->db->insert('user_group', $data); I tried both
-         $this->db->insert_batch('reporter', $data);
-        //$this->db->query('ALTER TABLE `reporter` ADD FOREIGN KEY(`id_news`) REFERENCES  ci_news(`ne_id`) ON DELETE CASCADE ON UPDATE CASCADE;');
+         //$this->db->insert('user_group', $data); 
+        $this->db->insert_batch('reporter', $data);
+        $this->db->query('ALTER TABLE `reporter` ADD FOREIGN KEY(`id_news`) REFERENCES  ci_news(`ne_id`) ON DELETE CASCADE ON UPDATE CASCADE;');
     }
     
     public function down() {
